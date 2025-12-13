1d4bffe0383ceedc50d96b1c6020ab8781946b4f
Sabarish.E <eswaransabarish604@gmail.com>
2025-12-13 14:43:12 +0530

    n

--- operators/q35.java
+++ operators/q35.java
@@ -5,7 +5,11 @@ public class q35
 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        int a= sc.nextInt();
-        System.err.println("");
+        double a= sc.nextDouble();
+        double b= sc.nextDouble();
+        double c=3.14*(a*a)*b;
+        double d=2*3.14*a*(a+b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
