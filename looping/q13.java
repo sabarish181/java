8cb6b33d0022843593542201f506dd9c46c5c0b6
Sabarish.E <eswaransabarish604@gmail.com>
2026-01-02 11:24:44 +0530

    n

--- looping/q13.java
+++ looping/q13.java
@@ -6,20 +6,20 @@ public class q13
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt(); 
+        int n = sc.nextInt(); 
         int i;
-        for (i = 0; i * i <= number; i++) 
+        for (i = 0; i * i <= n; i++) 
         {
-            if (i * i == number) 
+            if (i * i == n) 
             {
-                System.out.println(number + " is a perfect square.");
+                System.out.println(n + " is a perfect square.");
                 break; 
             }
         }
         
-        if (i * i > number) 
+        if (i * i > n) 
             { 
-            System.out.println(number + " is not a perfect square.");
+            System.out.println(n + " is not a perfect square.");
             }
     }
 }
